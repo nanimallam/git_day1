a953816f6ebd8dc3f5bc90e100072de587a38588
nanimallam <rahulmallam1432@gmail.com>
2026-07-22 21:36:01 +0530

    added new code

--- git.py
+++ git.py
@@ -1,2 +1,22 @@
 def welcome():
     print("welcome to git")
+def prime(n):
+    factors = 0
+
+    for i in range(1, n + 1):
+        if n % i == 0:
+            factors += 1
+
+    if factors == 2:
+        return True
+    else:
+        return False
+
+
+num = int(input())
+
+if prime(num):
+    print("Prime")
+else:
+    print("Not Prime")
+
